ca0fe06dfb086dd629ac8071f5b6fec78fa4a031
ayushs2725 <aysingh@ucsd.edu>
2022-04-29 19:35:36 -0700

    update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,21 +12,29 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            if(markdown.indexOf("(") == -1 || markdown.indexOf(")") == -1 || markdown.indexOf("]") == -1 || markdown.indexOf("[") == -1)
-            {
-                continue;
-            }
+            // if(markdown.indexOf("(") == -1 || markdown.indexOf(")") == -1 || markdown.indexOf("]") == -1 || markdown.indexOf("[") == -1)
+            // {
+            //     continue;
+            // }
 
-            else
-            {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            // else
+            // {
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+
+                if(markdown.indexOf("(") == -1 || markdown.indexOf(")") == -1 || markdown.indexOf("]") == -1 || markdown.indexOf("[") == -1)
+                {
+                    break;
+                }
+
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+
+                System.out.println(currentIndex);
             }
-        }
+        //}
 
         return toReturn;
     }
