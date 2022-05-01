f91abc1e90fde34c0121ab960b8663b964bd466e
ayushs2725 <aysingh@ucsd.edu>
2022-05-01 15:08:55 -0700

    fix fail 2

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -24,6 +24,10 @@ public class MarkdownParse {
                 }
 
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
+                if (closeParen + 1 < currentIndex)
+                {
+                    break;
+                }
                 currentIndex = closeParen + 1;
 
                 System.out.println(currentIndex);
@@ -33,6 +37,7 @@ public class MarkdownParse {
 
 
     public static void main(String[] args) throws IOException {
+
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
