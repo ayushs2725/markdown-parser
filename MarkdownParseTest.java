3d75e484d1382a64c0d444327e2dac2acdf8d50b
ayushs2725 <aysingh@ucsd.edu>
2022-05-01 14:26:47 -0700

    Reset to initial

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,16 +1,11 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
 public class MarkdownParseTest {
 
     @Test
     public void addition() {
-        assertEquals(3, 1 + 1);
-    }
-
-    @Test
-    public void infinite()
-    {
-        assertEquals("", MarkdownParse.getLinks("test-file7"));
+        assertEquals(2, 1 + 1);
     }
 
 }
