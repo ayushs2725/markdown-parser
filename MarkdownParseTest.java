9b2e0a3f211f3312156aaafaf4e12fa3261b6587
ayushs2725 <aysingh@ucsd.edu>
2022-05-23 17:09:53 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,11 +1,33 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
 
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void Snippet1() 
+    {
+        String arr1[] = {"'google.com", "google.com", "ucsd.edu"};
+
+        String arr2[] = MarkdownParse.getLinks(Files..)
+    }
+
+    @Test
+    public void Snippet2()
+    {
+        String arr1[] = {"b.com", "a.com(())", "example.com"};
+
+        String arr2[];
+    }
+
+    @Test
+    public void Snippet3()
+    {
+        String arr1[] = {};
     }
 
 }
